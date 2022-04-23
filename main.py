4358805fdb070665abb7995246427ab9a7058dd0
Og Azevedo <ogazevedo.py@gmail.com>
2022-04-23 09:33:04 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,9 @@
+#baixar o crhomedriver https://chromedriver.chromium.org/downloads
+from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+
+
+# servico = Service(ChromeDriverManager().install())
+navegador = webdriver.Chrome()
+ddd
